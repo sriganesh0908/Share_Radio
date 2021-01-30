a259a4df43079d43073ef0da52578e7b9e3a0627
sriganesh0908 <78194676+sriganesh0908@users.noreply.github.com>
2021-01-30 10:18:12 +0530

    Update config.php

--- Registration/config.php
+++ Registration/config.php
@@ -1,8 +1,8 @@
 <?php
 $dbhost='localhost';
-$dbname='sharerad_loginsystem';
-$dbusername='sharerad_sharera';
-$dbpassword='Ganesh@2000';
+$dbname='';
+$dbusername='';
+$dbpassword='';
 $dbc=mysqli_connect($dbhost,$dbusername,$dbpassword,$dbname);
 if(!$dbc){
 	   die("ERROR: could not connect." . mysqli_connect_error());
